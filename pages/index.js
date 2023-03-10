ba0fc393f3a7788e3b1c9ff513096878717665b5
tommy byrne <tommybyrne9@gmail.com>
2023-03-10 17:08:59 +0000

    clean project w/framer-motion

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+// components
+import Spotlight from "@/components/spotlight";
+
 const Home = () => {
-    return <div className="">Home</div>;
+    return (
+        <div className="relative flex h-screen w-screen items-center justify-center">
+            <Spotlight background="#000000" fromColor="#b4a3ea" toColor="#8b71de" />
+        </div>
+    );
 };
 
 export default Home;
